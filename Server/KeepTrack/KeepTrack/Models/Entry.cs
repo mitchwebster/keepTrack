e7e2cd2c9626454bb8f80b51df209ca55065d93f
Mitch Webster <mitchell.a.webster@gmail.com>
2018-12-09 16:11:30 -0800

    Merging test branch

--- Server/KeepTrack/KeepTrack/Models/Entry.cs
+++ Server/KeepTrack/KeepTrack/Models/Entry.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Web; // Some silly commit
+using System.Web;
 
 namespace KeepTrack.Models
 {
